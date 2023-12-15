7e2b912b0a41ba15689790855d31efcf6c385da2
satvik1508 <satviksharma1508@gmail.com>
2023-12-15 22:18:07 +0530

    first commit

---
+++ controllers/transactionCtrl.js
@@ -0,0 +1,64 @@
+const transactionModel = require("../models/transactionModel");
+const moment = require("moment");
+const getAlltransaction = async (req, res) => {
+  try {
+    const { frequency, selectedDate, type } = req.body;
+    const transactions = await transactionModel.find({
+      ...(frequency !== "custom"
+        ? {
+            date: {
+              $gt: moment().subtract(Number(frequency), "d").toDate(),
+            },
+          }
+        : {
+            date: {
+              $gte: selectedDate[0],
+              $lte: selectedDate[1],
+            },
+          }),
+      userid: req.body.userid,
+      ...(type !== "all" && { type }),
+    });
+    res.status(200).json(transactions);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(erorr);
+  }
+};
+
+const deletetransaction = async (req, res) => {
+  try {
+    await transactionModel.findOneAndDelete({ _id: req.body.transacationId });
+    res.status(200).send("Transaction Deleted!");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+const edittransaction = async (req,res) => {
+  try {
+    await transactionModel.findOneAndUpdate(
+      { _id: req.body.transacationId },
+      req.body.payload
+    );
+    res.status(200).send("Edit Successfully");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+const addtransaction = async (req, res) => {
+  try {
+    // const newtransaction = new transactionModel(req.body);
+    const newtransaction = new transactionModel(req.body);
+    await newtransaction.save();
+    res.status(201).send("transaction Created");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+module.exports = { getAlltransaction, addtransaction ,edittransaction,deletetransaction};
---
+++ routes/transactionRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const {
+  addtransaction,
+  getAlltransaction,
+  edittransaction,
+  deletetransaction
+} = require("../controllers/transactionCtrl");
+
+//router object
+const router = express.Router();
+
+//routes
+//add transaction POST MEthod
+router.post("/add-transaction", addtransaction);
+
+//edit transaction POST MEthod
+router.post("/edit-transaction", edittransaction);
+
+//delete transaction POST MEthod
+router.post("/delete-transaction", deletetransaction);
+
+//get transactions
+router.post("/get-transaction", getAlltransaction);
+
+module.exports = router;
